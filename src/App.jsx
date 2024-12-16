d1d590698dbfb20ddbc0dfc76e248b6667602b0b
nshimiyumukiza <nshimiyumukizaerneste99@gmail.com>
2024-12-16 15:23:38 +0200

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { BrowserRouter,Routes,Route } from "react-router";
+const App = () => {
+  return (
+   <BrowserRouter >
+   <Routes >
+<Route ></Route>
+   </Routes>
+   </BrowserRouter>
+  )
+}
+
+export default App
+
